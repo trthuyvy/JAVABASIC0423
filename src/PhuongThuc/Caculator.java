87ae11151b8be6e26761173e3953aa3ef06d1fc0
Vy <trthuyvy@gmail.com>
2023-04-24 23:49:38 +0700

    đẩy bt 3

--- src/PhuongThuc/Caculator.java
+++ src/PhuongThuc/Caculator.java
@@ -1,4 +1,4 @@
-package METHOD;
+package PhuongThuc;
 
 public class Caculator {
 
---
+++ src/ToanTu/IfElse.java
@@ -0,0 +1,20 @@
+package ToanTu;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 7;
+        int n = 4;
+
+        if(n == a){
+            System.out.println("Kết quả đúng");
+        } else if (n < b) {
+            System.out.println("Kết quả đúng");
+        }
+        else {
+            System.out.println("Kết quả không hợp lệ");
+        }
+    }
+
+}
