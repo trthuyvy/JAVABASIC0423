3d51fd1943e1e10dafc9c370e5a4bae85c2af859
Vy <trthuyvy@gmail.com>
2023-04-25 21:47:33 +0700

    Đẩy source code

--- src/BT3_IfElse/IfElse.java
+++ src/BT3_IfElse/IfElse.java
@@ -7,14 +7,15 @@ public class IfElse {
         int b = 7;
         int n = 4;
 
-        if(n == a){
-            System.out.println("Kết quả đúng");
-        } else if (n < b) {
-            System.out.println("Kết quả đúng");
-        }
-        else {
-            System.out.println("Kết quả không hợp lệ");
-        }
+        boolean checkN1 = ((a == n) && (n < b));
+        boolean checkN2 = ((a == n) && (n > b));
+
+        String  expectedText = "Giá trị n hợp lệ";
+        String actual = "Giá trị n hợp lệ";
+
+        System.out.println(checkN1);
+        System.out.println(checkN2);
+        System.out.println(expectedText == actual);
     }
 
 }
