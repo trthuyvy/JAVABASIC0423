3498466d08b05e0aad423e3e49b5b791e29cb3e1
Vy <trthuyvy@gmail.com>
2023-05-15 21:33:27 +0700

    Đẩy source code

--- src/BT5_Collection/BT_ArrayList.java
+++ src/BT5_Collection/BT_ArrayList.java
@@ -8,12 +8,12 @@ public class BT_ArrayList {
         //Khai báo ArrayList
         ArrayList<String> arrayList = new ArrayList<String>();
 
-        //Thêm thông tin nhân viên vào collection
+        //Thêm thông tin học viên vào collection
         arrayList.add("Họ tên: Thúy Vy");
         arrayList.add("Môn học: Selenium");
         arrayList.add("Khóa học: 042023");
 
-        //Duyệt ArrayList, in ra thông tin nhân viên
+        //Duyệt ArrayList, in ra thông tin học viên
         for (int i = 0; i < arrayList.size(); i++){
             System.out.println(arrayList.get(i));
         }
